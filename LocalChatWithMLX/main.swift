2d959c710c0cefea3bd8fe1518741882a115dbc6
Dmytro Hladkyi <dmytro.cpp@gmail.com>
2026-04-19 17:24:24 +0200

    Use stream response. streamDetails supports launching tools and provides info about reasponse

--- LocalChatWithMLX/main.swift
+++ LocalChatWithMLX/main.swift
@@ -13,12 +13,48 @@ import MLXHuggingFace
 import HuggingFace
 import Tokenizers
 
-let modelConfiguration = LLMRegistry.llama3_2_1B_4bit
+let modelConfiguration = LLMRegistry.llama3_2_3B_4bit
 
-let model = try await #huggingFaceLoadModelContainer(
+let modelContainer = try await #huggingFaceLoadModelContainer(
     configuration: modelConfiguration
 )
 
-let session = ChatSession(model)
-print(try await session.respond(to: "What are two things to see in San Francisco?"))
-print(try await session.respond(to: "How about a great place to eat?"))
+let session = ChatSession(modelContainer)
+
+func ask(_ prompt: String) async throws {
+    print("\nUser: \(prompt)")
+    print("Assistant: ", terminator: "")
+    for try await chunk in await session.streamResponse(to: prompt, images: [], videos: []) {
+        print(chunk, terminator: "")
+        fflush(stdout)
+    }
+    print()
+}
+
+func askWithDetails(_ prompt: String) async throws {
+    print("\nUser: \(prompt)")
+    print("Assistant: ", terminator: "")
+    var stats: GenerateCompletionInfo? = nil
+    for try await generation in await session.streamDetails(to: prompt, images: [], videos: []) {
+        if case .chunk(let text) = generation {
+            print(text, terminator: "")
+            fflush(stdout)
+        }
+        
+        if case .info(let info) = generation {
+            stats = info
+        }
+            
+    }
+    print()
+    
+    if let stats {
+        print(stats)
+    }
+    print()
+}
+
+
+try await askWithDetails("What are two things to see in San Francisco?")
+try await askWithDetails("How about a great place to eat?")
+try await askWithDetails("Yes")
