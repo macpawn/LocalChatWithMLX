31eca64dc43db54503b0de69c33c85fd6cd68b61
Dmytro Hladkyi <dmytro.cpp@gmail.com>
2026-04-26 23:49:42 +0200

    Quit app on window closing

--- LocalChat/LocalChatApp.swift
+++ LocalChat/LocalChatApp.swift
@@ -1,7 +1,14 @@
 import SwiftUI
+import AppKit
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool { true }
+}
 
 @main
 struct LocalChatApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
